6b7370717df496eeb6a8b97bbb14d7d4e76e77ce
AndrasiSandras <tothzsolt2002wot@gmail.com>
2024-10-23 00:45:19 +0200

    Staff, Client login

--- src/main/java/unideb/hu/SFMProject/LoginScene.java
+++ src/main/java/unideb/hu/SFMProject/LoginScene.java
@@ -1,4 +1,25 @@
 package unideb.hu.SFMProject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LoginScene {
+
+    @FXML
+    private Button ClientLoginButton;
+
+    @FXML
+    private Button StaffLoginButton;
+
+    @FXML
+    void staffLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clientLogin(ActionEvent event) {
+
+    }
+
 }
