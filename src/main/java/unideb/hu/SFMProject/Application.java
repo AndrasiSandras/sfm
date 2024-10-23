a5e5df2a24c1e828e2348d94440454d62480f93e
Knandi <kalmarnandor65@gmail.com>
2024-10-23 13:54:04 +0200

    Login ablak kész

--- src/main/java/unideb/hu/SFMProject/Application.java
+++ src/main/java/unideb/hu/SFMProject/Application.java
@@ -1,17 +1,49 @@
 package unideb.hu.SFMProject;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.h2.tools.Console;
 import org.h2.tools.Server;
 
-public class Application {
+import static javafx.application.Application.launch;
+
+public class Application extends javafx.application.Application{
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("FXMLLoginScene"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/view/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+
+
 
     public static void main(String[] args) throws SQLException {
+
+
+
         startDatabase();
+        launch();
 
         final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unideb.hu.SFMProject");
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
