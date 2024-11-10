e7386a8a84555130e1efa86671f55c0b8df983f1
AndrasiSandras <tothzsolt2002wot@gmail.com>
2024-11-10 13:05:00 +0100

    SceneController fix

--- src/main/java/unideb/hu/SFMProject/SceneController.java
+++ src/main/java/unideb/hu/SFMProject/SceneController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,66 +16,47 @@ public class SceneController {
     private Scene scene;
     private Parent root;
 
-    @FXML
-    void goBack(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLLoginScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    // General method to load a scene
+    private void loadScene(ActionEvent event, String fxmlPath) throws IOException {
+        root = FXMLLoader.load(getClass().getResource(fxmlPath));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    void goBack(ActionEvent event) throws IOException {
+        loadScene(event, "/view/FXMLLoginScene.fxml");
     }
 
     @FXML
     void goBackToLogin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLClientLoginScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLClientLoginScene.fxml");
     }
 
     @FXML
     void staffLogin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLStaffLoginScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLStaffLoginScene.fxml");
     }
 
     @FXML
     void clientLogin(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLClientLoginScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLClientLoginScene.fxml");
     }
 
     @FXML
     void logInStaff(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLStaffScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLStaffScene.fxml");
     }
 
     @FXML
     void logInClient(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLClientScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLClientScene.fxml");
     }
 
     @FXML
     void registerNewClient(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/view/FXMLClientRegisterScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        loadScene(event, "/view/FXMLClientRegisterScene.fxml");
     }
 }
